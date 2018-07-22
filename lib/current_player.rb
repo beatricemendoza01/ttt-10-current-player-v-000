6be67efae71f358894314e7c2c080581d05ebc04
Beatrice Mendoza <mendoza@nyu.edu>
2018-07-22 14:17:16 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   counter = 0
   board.each do |board_space|
-    if board_space == "x" || board_space == "o"
+    if board_space == "X" || board_space == "O"
       counter += 1
     end
   end
@@ -10,5 +10,8 @@ def turn_count(board)
 end
 
 def current_player(board)
-  
+  if turn_count(board) % 2 == 0
+    return "X"
+  elsif turn_count(board) 
+  end
 end
